91c56aa9314a8255ac36f6dad26d4db97e904f04
kodebae <heykarmendurbin@gmail.com>
2024-01-25 01:00:10 -0500

    chore: fixed typo added more styling

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php 
 require_once 'includes/header.php';
 ?>
+<div class="form-container">
+    <h1>Login</h1>
+    <p>Need a new account?<a href="register.php">Sign up here!</a></p>
 
-<h1>Login</h1>
-<p>Need a new account?<a href="register.php">Sign up here!</a></p>
-
-<form action="" method="post">
-    <input type="text" name="username" placeholder="username">
-    <input type="text" name="password" placeholder="password">
-    <input type="text" name="email" placeholder="email">
-    <button type="submit>Login</button>
-</form>
-
+    <form action="" method="post">
+        <input type="text" name="username" placeholder="username">
+        <input type="text" name="password" placeholder="password">
+        <input type="text" name="email" placeholder="email">
+        <button type="submit">Login</button>
+    </form>
+</div> 
 
 <?php 
 require_once 'includes/footer.php';
